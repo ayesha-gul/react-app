c8fd14f4bacedf56430e45b0a49aaf60993c6ed7
Ayesha <fxolbird@gmail.com>
2021-01-01 12:23:24 -0800

    node change

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hello to the world and React app . ..</h1>
+      <h1>hello world</h1>
     </div>
+      
+    
   );
 }
 
