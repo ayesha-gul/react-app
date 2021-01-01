0dc5de689c07a46b0e685ac4ee895df3a3b2802d
Ayesha <fxolbird@gmail.com>
2021-01-01 12:14:26 -0800

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hello to the world and React app..</h1>
+     <h1>Hello to the world and React app . ..</h1>
     </div>
   );
 }
